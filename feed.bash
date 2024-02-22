20c4789af0db40d5120665dca228dd2f238974db
Jochen Schalanda <jochen@schalanda.name>
2024-02-22 10:29:24 +0100

    Update page-data URL

--- feed.bash
+++ feed.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e -u -o pipefail
 
-curl -fsSO 'https://bell-sw.com/page-data/blog/page-data.json'
+curl -fsSO 'https://assets.bell-sw.com/www.bell-sw.com/page-data/blog/page-data.json'
 mkdir _site
 jq -f filter.jq page-data.json > _site/feed.json
